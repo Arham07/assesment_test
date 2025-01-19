1f81d1668f1fc33a09b4409de8b34ae63ff9a972
ArhamJaved <arham.javed@octdaily.com>
2025-01-19 16:34:49 +0500

    feat: Trending Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,25 @@
 import 'package:assesment_app/presentation/trending/trending_screen.dart';
 import 'package:assesment_app/presentation/trending/trending_view_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
+import 'configs/const/colors.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: primary.withOpacity(0.3),
+      statusBarBrightness: Brightness.light,
+      statusBarIconBrightness: Brightness.dark,
+      systemStatusBarContrastEnforced: true,
+    ),
+  );
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   runApp(const MyApp());
 }
 
@@ -19,7 +33,7 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (context) => TrendingViewModel()),
       ],
       child: ScreenUtilInit(
-          designSize: const Size(480, 854),
+          designSize: const Size(411.4, 867.4),
           useInheritedMediaQuery: true,
           minTextAdapt: true,
           builder: (context, child) {
